a03ed54aa0ff76de3087b2b279f076e86faa6965
Emma <969118919@qq.com>
2019-06-21 18:36:40 +0800

    '按钮测试'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Button } from 'antd';
+/*
+应用根组件
+ */
+class App extends Component {
+    render(){
+        return<div>
+            <Button type='primary'>按钮</Button>
+        </div>
+    }
+}
+
+
+export default App
